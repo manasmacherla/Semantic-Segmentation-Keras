2a82dcf014355fe86a1121bc4e2468b5b782631a
Manas Macherla <rmacher@g.clemson.edu>
2020-10-05 18:42:31 -0400

    implemented encoder for vgg16

--- model.py
+++ model.py
@@ -12,3 +12,35 @@ def vgg_encoder(input_height = 224, input_width = 224, weights = 'imagenet'):
     x = MaxPooling2D((2,2), strides=(2,2), name = 'pool_layer1')(x)
 
     layer1 = x
+
+    x = Conv2D(128, (3,3), activation = 'relu', padding = 'same', name = 'conv1_layer2')(x)
+    x = Conv2D(128, (3,3), activation = 'relu', padding = 'same', name = 'conv2_layer2')(x)
+    x = MaxPooling2D((2,2), strides=(2,2), name = 'pool_layer2')(x)
+
+    layer2 = x
+
+    x = Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'conv1_layer3')(x)
+    x = Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'conv2_layer3')(x)
+    x = Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'conv3_layer3')(x)
+    x = MaxPooling2D((2,2), strides=(2,2), name = 'pool_layer3')(x)
+
+    layer3 = x
+
+    x = Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'conv1_layer4')(x)
+    x = Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'conv2_layer4')(x)
+    x = Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'conv3_layer4')(x)
+    x = MaxPooling2D((2,2), strides=(2,2), name = 'pool_layer4')(x)
+
+    layer4 = x
+
+    x = Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'conv1_layer5')(x)
+    x = Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'conv2_layer5')(x)
+    x = Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'conv3_layer5')(x)
+    x = MaxPooling2D((2,2), strides=(2,2), name = 'pool_layer5')(x)
+
+    layer5 = x
+
+    
+
+
+
