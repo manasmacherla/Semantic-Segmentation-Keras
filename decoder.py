5f88b7c72a6f9c45bebe75d5021eeab42e945924
Manas Macherla <rmacher@g.clemson.edu>
2020-10-05 23:02:13 -0400

    FCN-8 implemented but need to check for bugs

--- decoder.py
+++ decoder.py
@@ -16,3 +16,23 @@ def decoder(classes, input_height = 416, input_width = 608):
     y = Dropout(0.5)(y)
     y = Conv2D(classes, (1,1), kernel_initializer='he_normal')(y)    
 
+    y = Conv2DTranspose(classes, kernel_size=(4,4), stride=(2,2), use_bias=False)(y)
+    layer4 = l4
+    layer4 = Conv2D(classes, (1,1), kernel_initializer='he_normal')
+    y = Add()([y, layer4]) #they are FCN-16
+
+    y = Conv2DTranspose(classes, kernel_size=(4,4), stride=(2,2), use_bias=False)(y)
+    layer3 = l3
+    layer3 = Conv2D(classes, (1,1), kernel_initializer='he_normal')
+    y = Add()([y, layer3])
+
+    y = Conv2DTranspose(classes, kernel_size=(16,16), stride=(8,8), use_bias=False)(y)
+
+    y = (Activation('softmax'))(y)
+
+    return y
+
+
+
+
+
